246ca5ca898699d00551c4a84a043e0f71b92c09
Eric O'Brien <obrienjeric@gmail.com>
2021-10-12 16:23:16 -0400

    adding rollbar instructions

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const Rollbar = require('rollbar');
+
+const rollbar = new Rollbar( {
+    accessToken: '',
+    captureUncaught: true,
+    captureUnhandledRejections: true
+});
+
+const app = express();
+
+app.use(express.json());
+app.use(rollbar.errorHandler());
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+    rollbar.info('html file served successfully');
+});
+
+app.use('/style', express.static(path.join(__dirname, './public/stlyesheet.css')));
+
+const port = process.env.PORT || 4404;
+
+app.listen(port, () => { console.log(`Server is up on ${port}`)});
