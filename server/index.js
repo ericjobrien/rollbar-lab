f4ab99c270110d465b39496f59ad0877ea4d2c9e
Eric O'Brien <obrienjeric@gmail.com>
2021-10-12 16:36:58 -0400

    messing with css

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.get('/', (req, res) => {
     rollbar.info('html file served successfully');
 });
 
-app.use('/stylesheet.css', express.static(path.join(__dirname, '../public/stlyesheet.css')));
+app.use('/css', express.static(path.join(__dirname, '../public/stylesheet.css')));
 
 const port = process.env.PORT || 4404;
 
